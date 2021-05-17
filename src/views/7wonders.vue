2635bba721a3fb791c84ad3c6df50587e4753ea7
Barbara Zeljeznik <barbara.zeljeznik@gmail.com>
2021-05-17 11:30:28 +0200

    Frontend-prototip

--- src/views/7wonders.vue
+++ src/views/7wonders.vue
@@ -74,7 +74,9 @@
 
 <div class="row">
   <div id="admin" class="col s12">
+    <br><br><br>
     <div class="card material-table">
+      
       <div class="table-header">
         <span class="table-title">Turniri</span>
         <div class="actions">
@@ -86,21 +88,33 @@
         <thead>
           <tr>
             <th>Email</th>
-            <th>Lokacija</th>
+            <th>Grad</th>
+            <th>Adresa</th>
             <th>Vrijeme</th>
             <th>Kratki opis</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Tiger Nixon</td>
-            <td>System Architect</td>
-            <td>Edinburgh</td>
+            <td>jan.2dan@gmail.com</td>
+            <td>Varaždin</td>
+            <td>Zagrebačka 300</td>
+            <td>13.00h</td>
+            <td>16+ godina</td>
           </tr>
           <tr>
-            <td>Garrett Winters</td>
-            <td>Accountant</td>
-            <td>Tokyo</td>
+            <td>kiki.igre@gmail.com</td>
+            <td>Rijeka</td>
+            <td>Matoševa 12</td>
+            <td>18.00h</td>
+            <td>Poneti figurice</td>
+          </tr>
+          <tr>
+            <td>miki70@gmail.com</td>
+            <td>OSijek</td>
+            <td>Žito 20</td>
+            <td>18.30h</td>
+            <td>Piće i hrana besplatni</td>
           </tr>
         </tbody>
       </table>
@@ -311,7 +325,210 @@ $(document).ready(function() {
 
 
 <style >
+.row{
+  margin:auto;
+  max-width:100%;
+}
 
+.custom-toggler .navbar-toggler-icon {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg%27%3E%3Cpath stroke='white' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+}
+
+.custom-toggler.navbar-toggler {
+  border-color: white;
+}
+body {
+	font-family: 'Varela Round', sans-serif;
+}
+.form-control {
+	box-shadow: none;		
+	font-weight: normal;
+	font-size: 13px;
+}
+.navbar {
+	background-color: black;
+	padding-left: 16px;
+	padding-right: 16px;
+	border-radius: 0;
+	
+}
+.navigacija{
+	position:fixed;
+	z-index:99;
+	width:100%;
+}
+.nav-link img {
+	border-radius: 50%;
+	width: 36px;
+	height: 36px;
+	margin: -8px 0;
+	float: left;
+	margin-right: 10px;
+}
+.logo {
+	padding-left: 0;
+	font-size: 20px;
+	padding-right: 50px;
+	height: 35px;
+	width: 190px;
+}
+.navbar .form-inline {
+	display: inline-block;
+}
+.navbar a {
+	color: white;
+	font-size: 15px;
+}
+.search-box {
+	position: relative;
+}	
+.search-box input {
+	padding-right: 35px;
+	border-color: #dfe3e8;
+	border-radius: 4px !important;
+	box-shadow: none
+}
+.search-box .input-group-text {
+	min-width: 35px;
+	border: none;
+	background: transparent;
+	position: absolute;
+	right: 0;
+	z-index: 9;
+	padding: 7px;
+	height: 100%;
+}
+.search-box i {
+	color: #a0a5b1;
+	font-size: 19px;
+}
+.navbar .sign-up-btn {
+	min-width: 110px;
+	max-height: 36px;
+}
+.navbar .dropdown-menu {
+	color: white;
+	font-weight: normal;
+	border-radius: 1px;
+	border-color: #e5e5e5;
+	box-shadow: 0 2px 8px rgba(0,0,0,.05);
+}
+.navbar a, .navbar a:active {
+	color: white;
+	padding: 8px 20px;
+	background: transparent;
+	line-height: normal;
+}
+.navbar .navbar-form {
+	border: none;
+}
+.navbar .action-form {
+	width: 280px;
+	padding: 20px;
+	left: auto;
+	right: 0;
+	font-size: 14px;
+}
+.navbar .action-form a {		
+	color: rgb(231, 231, 29);	
+	padding: 0 !important;
+	font-size: 14px;
+}
+.navbar .action-form .hint-text {
+    text-align: center;
+    margin-bottom: 15px;
+    font-size: 13px;
+}
+.navbar .btn-primary, .navbar .btn-primary:active {
+	color: #fff;
+	background:  rgb(231, 231, 29)	 !important;
+	border: none;
+}	
+.navbar .btn-primary:hover, .navbar .btn-primary:focus {		
+	color: #fff;
+	background: rgb(231, 231, 29)	 !important;
+}
+.navbar .social-btn .btn, .navbar .social-btn .btn:hover {
+	color: #fff;
+	margin: 0;
+	padding: 0 !important;
+	font-size: 13px;
+	border: none;
+	transition: all 0.4s;
+	text-align: center;
+	line-height: 34px;
+	width: 47%;
+	text-decoration: none;
+}
+.navbar .social-btn .facebook-btn {
+	background: yellow;
+}
+.navbar .social-btn .facebook-btn:hover {
+	background: #4676bd;
+}
+.navbar .social-btn .twitter-btn {
+	background: #64ccf1;
+}
+.navbar .social-btn .twitter-btn:hover {
+	background: #4ec7ef;
+}
+.navbar .social-btn .btn i {
+	margin-right: 5px;
+	font-size: 16px;
+	position: relative;
+	top: 2px;
+}
+.or-seperator {
+	margin-top: 32px;
+	text-align: center;
+	border-top: 1px solid #e0e0e0;
+}
+.or-seperator b {
+	color: #666;
+	padding: 0 8px;
+	width: 30px;
+	height: 30px;
+	font-size: 13px;
+	text-align: center;
+	line-height: 26px;
+	background: #fff;
+	display: inline-block;
+	border: 1px solid #e0e0e0;
+	border-radius: 50%;
+	position: relative;
+	top: -15px;
+	z-index: 1;
+}
+.navbar .action-buttons .dropdown-toggle::after {
+	display: none;
+}
+.form-check-label input {
+	position: relative;
+	top: 1px;
+}
+.ck{
+	padding-bottom:100px;
+}
+@media (min-width: 1200px){
+	.form-inline .input-group {
+		width: 300px;
+		margin-left: 30px;
+	}
+}
+@media (max-width: 768px){
+	.navbar .dropdown-menu.action-form {
+		width: 100%;
+		padding: 10px 15px;
+		background: transparent;
+		border: none;
+	}
+	.navbar .form-inline {
+		display: block;
+	}
+	.navbar .input-group {
+		width: 100%;
+	}
+}
 div.material-table {
   padding: 0;
 }
